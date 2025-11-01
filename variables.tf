dd82c630131c4d830bdc4d5b8703a0f92bbd6d30
Guillermo Dotti <diottiguillermo@gmail.com>
2025-11-01 14:24:07 -0300

    Uploading first changes

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_region" {
+  description = "Región de AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "github_token" {
+  description = "Personal Access Token de GitHub"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_repository" {
+  description = "URL del repositorio de GitHub"
+  type        = string
+  default     = "https://github.com/GuillermoDiotti/my-cloud-react-page"
+}
+
+variable "app_name" {
+  description = "Nombre de la aplicación en Amplify"
+  type        = string
+  default     = "my-cloud-react-app"
+}
+
+variable "branch_name" {
+  description = "Nombre de la rama principal"
+  type        = string
+  default     = "main"
+}
+
